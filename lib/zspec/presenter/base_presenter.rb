9ae5448ab9f13938b0622b2a0c3b6ab507a55d20
Eric Abrahamsen <erica@zillowgroup.com>
2019-10-02 14:41:28 -0400

    Remove puts and fix flaky duration

--- lib/zspec/presenter/base_presenter.rb
+++ lib/zspec/presenter/base_presenter.rb
@@ -44,10 +44,9 @@ module ZSpec
         end
 
         puts "FLAKY SPECS:"
-        puts ZSpec.config.queue.flaky_specs
         ZSpec.config.queue.flaky_specs.take(ZSpec.config.failure_count).each do |failure|
           puts "#{failure["message"]} failed #{failure["count"]} times. " \
-               "last failure was #{humanize(failure["last_failure"])} ago.\n"
+            "last failure was #{humanize(Time.now.to_i - failure["last_failure"])} ago.\n"
         end
 
         $stdout.flush
