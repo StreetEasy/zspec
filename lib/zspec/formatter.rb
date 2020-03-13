ca223541167dc71ce1c514f3f7cdf6921f482587
Seth Pollack <seth@sethpollack.net>
2020-03-13 13:53:29 -0400

    track failures outside of examples

--- lib/zspec/formatter.rb
+++ lib/zspec/formatter.rb
@@ -4,7 +4,7 @@ module ZSpec
   class Formatter < ::RSpec::Core::Formatters::BaseFormatter
     def initialize(queue:, tracker:, stdout:, message:)
       super
-      @output_hash = { failures: [] }
+      @output_hash = { }
       @failed      = false
       @message     = message
       @queue       = queue
@@ -15,7 +15,7 @@ module ZSpec
 
     def example_failed(failure)
       @failed = true
-      @output_hash[:failures] << format_example(failure.example)
+      (@output_hash[:failures] ||= []) << format_example(failure.example)
     end
 
     def dump_summary(summary)
@@ -33,7 +33,7 @@ module ZSpec
         @stdout.string
       )
       @tracker.track_runtime(@message, @duration)
-      @tracker.track_failures(@output_hash[:failures]) if @failed
+      @tracker.track_failures(@output_hash[:failures] || [id: @message]) if @failed
       @tracker.track_sequence(@message)
       log_trace_info
     end
